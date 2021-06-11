b7a92667cfd9b366b7e81865f1c23f595837d757
joy-rosie <joytolia@hotmail.com>
2021-06-11 10:10:18 +0100

    add some initial factory methods, getters, copy

--- lib/src/main/java/agent/smith/Matrix.java
+++ lib/src/main/java/agent/smith/Matrix.java
@@ -1,39 +1,56 @@
 package agent.smith;
 
 
+// Copying arrays:
+// https://stackoverflow.com/questions/1697250/difference-between-various-array-copy-methods
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Matrix {
 
-    private final double [][] array;
+    private final double[] array;
     private final int numRows;
     private final int numCols;
+    private final int length;
 
-    private static double[][] copyArray(double[][] array, int numRows, int numCols) {
-        double[][] arrayCopy = new double[numRows][numCols];
-        for (int i=0; i < numRows; i++) {
-//            if (array[i].length != numCols) {
-//                throw new AssertionError("Inconsistent row sizes");
-//            }
-            // Copy array to make sure we do not reference input array
-            // https://stackoverflow.com/questions/1697250/difference-between-various-array-copy-methods
-            System.arraycopy(array[i], 0, arrayCopy[i], 0, numCols);
+    private Matrix(double[] array, int numRows, int numCols) {
+
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.length = this.numRows * this.numCols;
+
+        if (this.numRows < 0) {
+            throw new IllegalArgumentException(
+                    String.format("'numRows' (%d) has to be a non negative integer", this.numRows));
         }
-        return arrayCopy;
+
+        if (this.numCols < 0) {
+            throw new IllegalArgumentException(
+                    String.format("'numCols' (%d) has to be a non negative integer", this.numCols));
+        }
+
+        if (array == null) {
+            throw new NullPointerException("'array' cannot be null");
+        }
+
+        if (array.length != this.length) {
+            throw new IllegalArgumentException(String.format(
+                    "Length of 'array' (%d) does not match 'numRows' * 'numCols' (%d)", array.length, this.length));
+        }
+
+        this.array = new double[this.length];
+        System.arraycopy(array, 0, this.array, 0, this.length);
     }
 
-    public Matrix(Matrix matrix) {
-        numRows = matrix.getNumRows();
-        numCols = matrix.getNumCols();
-        array = copyArray(matrix.viewArray(), numRows, numCols);
+    public static Matrix fromArray(double[] array, int numRows, int numCols) {
+        return new Matrix(array, numRows, numCols);
     }
 
-    public Matrix(double[][] array) {
-        numRows = array.length;
-        numCols = array[0].length;
-        this.array = copyArray(array, numRows, numCols);
-    }
-
-    public double[][] viewArray() {
-        return array;
+    public double[] getArray() {
+        double[] newArray = new double[length];
+        System.arraycopy(array, 0, newArray, 0, length);
+        return newArray;
     }
 
     public int getNumRows() {
@@ -42,5 +59,53 @@ public class Matrix {
 
     public int getNumCols() {
         return numCols;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Matrix matrix = (Matrix) o;
+        return numRows == matrix.numRows && numCols == matrix.numCols && Arrays.equals(array, matrix.array);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(numRows, numCols);
+        result = 31 * result + Arrays.hashCode(array);
+        return result;
+    }
+
+    public static Matrix fromNestedArray(double[][] nestedArray) {
+
+        if (nestedArray == null) {
+            throw new NullPointerException("'nestedArray' cannot be null");
+        }
+
+        if (nestedArray[0] == null) {
+            throw new NullPointerException("'nestedArray[0]' cannot be null");
+        }
+
+        int numRows = nestedArray.length;
+        int numCols = nestedArray[0].length;
+
+        double[] array = new double[numRows * numCols];
+        for (int i=0; i < numRows; i++) {
+
+            if (nestedArray[i] == null) {
+                throw new NullPointerException(String.format("'nestedArray[%d]' cannot be null", i));
+            }
+
+            if (nestedArray[i].length != numCols) {
+                throw new IllegalArgumentException("Inconsistent number of rows for 'nestedArray'");
+            }
+
+            System.arraycopy(nestedArray[i], 0, array, i * numCols, numCols);
+        }
+        return fromArray(array, numRows, numCols);
+    }
+
+    public Matrix copy() {
+        return fromArray(array, numRows, numCols);
     }
 }
--- lib/src/test/java/agent/smith/MatrixTest.java
+++ lib/src/test/java/agent/smith/MatrixTest.java
@@ -5,19 +5,394 @@ package agent.smith;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MatrixTest {
 
     @Test
-    public void testMatrixConstructorMatrix() {
-        Matrix M = new Matrix(new double[][]{{1, 2}, {3, 4}});
-        double[][] expected = {{1, 2}, {3, 4}};
+    public void testMatrixFactoryFromArrayNegativeNumRows() {
+        double[] array = new double[]{};
+        int numRows = -1;
+        int numCols = 0;
+        String expected = "'numRows' (-1) has to be a non negative integer";
 
-        Matrix matrix = new Matrix(M);
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromArray(array, numRows, numCols));
 
-        assertArrayEquals(expected, matrix.viewArray());
+        assertEquals(expected, thrown.getMessage());
     }
+
+    @Test
+    public void testMatrixFactoryFromArrayNegativeNumCols() {
+        double[] array = new double[]{};
+        int numRows = 0;
+        int numCols = -1;
+        String expected = "'numCols' (-1) has to be a non negative integer";
+
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromArray(array, numRows, numCols));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromArrayExceptionNull() {
+        double[] array = null;
+        int numRows = 0;
+        int numCols = 0;
+        String expected = "'array' cannot be null";
+
+        Exception thrown = assertThrows(NullPointerException.class, () -> Matrix.fromArray(array, numRows, numCols));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromArrayExceptionLengthTooLow() {
+        double[] array = new double[]{};
+        int numRows = 1;
+        int numCols = 1;
+        String expected = "Length of 'array' (0) does not match 'numRows' * 'numCols' (1)";
+
+        Exception thrown = assertThrows(
+                IllegalArgumentException.class, () -> Matrix.fromArray(array, numRows, numCols));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromArrayExceptionLengthTooHigh() {
+        double[] array = new double[]{1, 2, 3, 4};
+        int numRows = 1;
+        int numCols = 1;
+        String expected = "Length of 'array' (4) does not match 'numRows' * 'numCols' (1)";
+
+        Exception thrown = assertThrows(
+                IllegalArgumentException.class, () -> Matrix.fromArray(array, numRows, numCols));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromArrayEmpty() {
+        double[] array = new double[]{};
+        int numRows = 0;
+        int numCols = 0;
+        double[] expectedArray = new double[]{};
+        int expectedNumRows = 0;
+        int expectedNumCols = 0;
+
+        Matrix matrix = Matrix.fromArray(array, numRows, numCols);
+
+        assertArrayEquals(matrix.getArray(), expectedArray, 0.0);
+        assertEquals(matrix.getNumRows(), expectedNumRows);
+        assertEquals(matrix.getNumCols(), expectedNumCols);
+    }
+
+    @Test
+    public void testMatrixFactoryFromArraySimple1() {
+        double[] array = new double[]{1, 2, 3, 4};
+        int numRows = 2;
+        int numCols = 2;
+        double[] expectedArray = new double[]{1, 2, 3, 4};
+        int expectedNumRows = 2;
+        int expectedNumCols = 2;
+
+        Matrix matrix = Matrix.fromArray(array, numRows, numCols);
+
+        assertArrayEquals(matrix.getArray(), expectedArray, 0.0);
+        assertEquals(matrix.getNumRows(), expectedNumRows);
+        assertEquals(matrix.getNumCols(), expectedNumCols);
+    }
+
+    @Test
+    public void testMatrixFactoryFromArraySimple2() {
+        double[] array = new double[]{1, 2, 3, 4, 5, 6};
+        int numRows = 2;
+        int numCols = 3;
+        double[] expectedArray = new double[]{1, 2, 3, 4, 5, 6};
+        int expectedNumRows = 2;
+        int expectedNumCols = 3;
+
+        Matrix matrix = Matrix.fromArray(array, numRows, numCols);
+
+        assertArrayEquals(matrix.getArray(), expectedArray, 0.0);
+        assertEquals(matrix.getNumRows(), expectedNumRows);
+        assertEquals(matrix.getNumCols(), expectedNumCols);
+    }
+
+    @Test
+    public void testMatrixFactoryFromArraySimple3() {
+        double[] array = new double[]{1, 2, 3, 4, 5, 6};
+        int numRows = 3;
+        int numCols = 2;
+        double[] expectedArray = new double[]{1, 2, 3, 4, 5, 6};
+        int expectedNumRows = 3;
+        int expectedNumCols = 2;
+
+        Matrix matrix = Matrix.fromArray(array, numRows, numCols);
+
+        assertArrayEquals(matrix.getArray(), expectedArray, 0.0);
+        assertEquals(matrix.getNumRows(), expectedNumRows);
+        assertEquals(matrix.getNumCols(), expectedNumCols);
+    }
+
+    @Test
+    public void testMatrixEquals1() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        Matrix objectToCompare = null;
+        boolean expected = false;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals2() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        Matrix objectToCompare = Matrix.fromArray(new double[]{}, 0, 0);
+        boolean expected = true;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals3() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        Matrix objectToCompare = matrix;
+        boolean expected = true;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals4() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        Matrix objectToCompare = Matrix.fromArray(new double[]{1}, 1, 1);
+        boolean expected = false;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals5() {
+        Matrix matrix = Matrix.fromArray(new double[]{0}, 1, 1);
+        Matrix objectToCompare = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        boolean expected = false;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals6() {
+        Matrix matrix = Matrix.fromArray(new double[]{0, 1}, 2, 1);
+        Matrix objectToCompare = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        boolean expected = false;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals7() {
+        Matrix matrix = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        Matrix objectToCompare = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        boolean expected = true;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixEquals8() {
+        Matrix matrix = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        double[] objectToCompare = new double[]{0, 1};
+        boolean expected = false;
+
+        boolean equals = matrix.equals(objectToCompare);
+
+        assertEquals(equals, expected);
+    }
+
+    @Test
+    public void testMatrixHashCode1() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        int expected = 29792;
+
+        int hashCode = matrix.hashCode();
+
+        assertEquals(hashCode, expected);
+    }
+
+    @Test
+    public void testMatrixHashCode2() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0, 0);
+        int expected = 29792;
+
+        int hashCode = matrix.hashCode();
+
+        assertEquals(hashCode, expected);
+    }
+
+    @Test
+    public void testMatrixHashCode3() {
+        Matrix matrix = Matrix.fromArray(new double[]{0, 1}, 1, 2);
+        int expected = 1072725023;
+
+        int hashCode = matrix.hashCode();
+
+        assertEquals(hashCode, expected);
+    }
+
+    @Test
+    public void testMatrixHashCode4() {
+        Matrix matrix = Matrix.fromArray(new double[]{0, 1}, 2, 1);
+        int expected = 1072725953;
+
+        int hashCode = matrix.hashCode();
+
+        assertEquals(hashCode, expected);
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionNull() {
+        double[][] nestedArray = null;
+        String expected = "'nestedArray' cannot be null";
+
+        Exception thrown = assertThrows(NullPointerException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionNestedNull0() {
+        double[][] nestedArray = {null};
+        String expected = "'nestedArray[0]' cannot be null";
+
+        Exception thrown = assertThrows(NullPointerException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionNestedNull1() {
+        double[] array = null;
+        double[][] nestedArray = {{0}, array};
+        String expected = "'nestedArray[1]' cannot be null";
+
+        Exception thrown = assertThrows(NullPointerException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionInconsistentNumRows1() {
+        double[][] nestedArray = {{0}, {}};
+        String expected = "Inconsistent number of rows for 'nestedArray'";
+
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionInconsistentNumRows2() {
+        double[][] nestedArray = {{0}, {1, 2}};
+        String expected = "Inconsistent number of rows for 'nestedArray'";
+
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionInconsistentNumRows3() {
+        double[][] nestedArray = {{}, {0}};
+        String expected = "Inconsistent number of rows for 'nestedArray'";
+
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayExceptionInconsistentNumRows4() {
+        double[][] nestedArray = {{1, 2}, {0}};
+        String expected = "Inconsistent number of rows for 'nestedArray'";
+
+        Exception thrown = assertThrows(IllegalArgumentException.class, () -> Matrix.fromNestedArray(nestedArray));
+
+        assertEquals(expected, thrown.getMessage());
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArrayEmpty() {
+        double[][] array = new double[][]{{}};
+        double[] expected = new double[]{};
+
+        Matrix matrix = Matrix.fromNestedArray(array);
+
+        assertArrayEquals(expected, matrix.getArray(), 0.0);
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArraySimple1() {
+        double[][] array = new double[][]{{1, 2}, {3, 4}};
+        Matrix expected = Matrix.fromArray(new double[]{1, 2, 3, 4}, 2, 2);
+
+        Matrix matrix = Matrix.fromNestedArray(array);
+
+        assertEquals(matrix, expected);
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArraySimple2() {
+        double[][] array = new double[][]{{1, 3}, {2, 4}};
+        Matrix expected = Matrix.fromArray(new double[]{1, 3, 2, 4}, 2, 2);
+
+        Matrix matrix = Matrix.fromNestedArray(array);
+
+        assertEquals(matrix, expected);
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArraySimple3() {
+        double[][] array = new double[][]{{1, 2, 3}, {4, 5, 6}};
+        Matrix expected = Matrix.fromArray(new double[]{1, 2, 3, 4, 5, 6}, 2, 3);
+
+        Matrix matrix = Matrix.fromNestedArray(array);
+
+        assertEquals(matrix, expected);
+    }
+
+    @Test
+    public void testMatrixFactoryFromNestedArraySimple4() {
+        double[][] array = new double[][]{{1, 2}, {3, 4}, {5, 6}};
+        Matrix expected = Matrix.fromArray(new double[]{1, 2, 3, 4, 5, 6}, 3, 2);
+
+        Matrix matrix = Matrix.fromNestedArray(array);
+
+        assertEquals(matrix, expected);
+    }
+
+    @Test
+    public void testMatrixCopy() {
+        Matrix matrix = Matrix.fromArray(new double[]{}, 0 ,0);
+
+        Matrix copiedMatrix = matrix.copy();
+
+        assertEquals(copiedMatrix, matrix);
+        assertNotSame(copiedMatrix, matrix);
+    }
+
 }
 
