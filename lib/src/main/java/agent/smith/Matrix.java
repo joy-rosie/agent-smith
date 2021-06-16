bc5ed024597466941947ee59d956491cc16a0dce
joy-rosie <joytolia@hotmail.com>
2021-06-16 16:52:25 +0100

    minor change

--- lib/src/main/java/agent/smith/Matrix.java
+++ lib/src/main/java/agent/smith/Matrix.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.lang.Math;
 import java.util.Random;
-import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
 
 public class Matrix {
 
